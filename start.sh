48aeea7aee9560810cd2777fe41c0b617c6b8619
Chinna Durai <61374961+durai5934@users.noreply.github.com>
2023-01-18 13:57:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vinarasan12/Natasha88 /DQ-The-File-Donor
+  git clone https://github.com/durai5934/Narutoo...- /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
